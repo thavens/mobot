5bf1f949c694a8f53f37b5fb191e47d1d51e72c3
Michael Lavery <michael.lavery.2017@gmail.com>
2023-01-29 19:59:08 -0800

    bugfix

--- options.py
+++ options.py
@@ -1,9 +1,9 @@
 DIRECT_SOCKET = False # don't use the forwarding server?
 VIDEO = False # Use webcam? Identical option in client.py
 SPEEDRATIO = 0.25 #0.48 # cap max speed 0-1
-JOY_DEADBAND = 30 / 1000 # joystick threshold for zero
+JOY_DEADBAND = 50 / 1000 # joystick threshold for zero
 CONTROL_HEADER = 'con:'.encode('ascii')
-WHEEL_RADIUS = 6.5 # inches
+WHEEL_RADIUS = 6.5 / 2 # inches
 ACCEL_MAX = 6 # m/s^2
 ALPHA_MAX = 1 # rad/s
 CONTROL_SEND_FREQ = 60
